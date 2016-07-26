3218729e30316b1714896df4105fb203e8c551d7
timoftealexandru <timofte.alexandru05@yahoo.com>
2016-07-26 12:37:26 +0300

    added filter for the directory json in react

--- src/App.jsx
+++ src/App.jsx
@@ -2,74 +2,99 @@ import React, {Component} from 'react';
 
 class File extends Component {
 
-  render(){
+    render() {
 
-    return(
-        <li className='file-item'>{this.props.name}</li>
-    );
-  }
+        return (
+            <li className='file-item'>{this.props.name}</li>
+        );
+    }
 }
 class Folder extends Component {
+    render=()=>{
 
-  render(){
-
-    return(
-        <li className='folder-item'>{this.props.name}</li>
-    );
-  }
-}
-class Input extends Component{
-  handleChange(event) {
-    console.log(this.state.value);
-}
-  getInitialState() {
-  return {value: ''};
-}
-  render(){
-
-    return (
-      <input
-          type="text"
-          placeholder="filter..."
-          valueLink={this.linkState('value')}
-          onChange={this.handleChange}
-      />
-    );
-  }
+        return (
+            <li className='folder-item'>{this.props.name}</li>
+        );
+    };
 }
 class FolderContainer extends Component {
-  render() {
-    const items = this.props.data;
-    let folderItems = [];
-    for(let i=0;i<items.length;i++) {
-          if(items[i].type === 'dir') {
-            folderItems.push(<Folder name={items[i].name} />);
-          } else {
-            folderItems.push(
-                <File name={items[i].name} />
-            );
-          }
-          if(items[i].children) {
-            folderItems.push(<FolderContainer  data={items[i].children}/>);
-          }
+
+    render() {
+        const items = this.props.data;
+        let folderItems = [];
+        let index=0;
+        for (let i = 0; i < items.length; i++) {
+            if (items[i].type === 'dir') {
+                folderItems.push(<Folder key={index++} name={items[i].name}/>);
+            } else {
+                folderItems.push(
+                    <File key={index++} name={items[i].name}/>
+                );
+            }
+            if (items[i].children) {
+                folderItems.push(<FolderContainer key={index++} data={items[i].children}/>);
+            }
+        }
+
+        return (
+            <ul>
+                {folderItems}
+            </ul>
+        );
     }
-    return (
-        <ul>
-          {folderItems}
-        </ul>
-    );
-  }
+}
+
+class Input extends Component{
+    render(){
+        return(
+            <div className="widget" onChange={this.props.handleChange}>
+              <input
+                    type="text"
+                   placeholder="filter..."
+              />
+            </div>
+        );
+    }
 }
 class App extends Component {
-  render() {
-    return (
-        <div className="widget">
-          <Input />
-          <div>{ this.props.value ? 'Searching for:' + this.props.value : null }</div>
-          <FolderContainer data={this.props.data} />
-        </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.handleInput=this.handleInput.bind(this);
+        this.state={
+            data:this.props.data
+        };
+    }
+    transformJSON=(data, value)=>{
+    let newObj=[];
+        (function filterJSON(obj, searchValue)
+        {
+            if(obj.name && (obj.name.indexOf(searchValue) >-1)){
+                newObj.push(obj);
+                return
+            }
+            for (var key in obj)
+            {
+                if (typeof obj[key] == "object" && obj[key] !== null){
+                    filterJSON(obj[key],searchValue);
+                }
+            }
+        })(data, value);
+        return newObj;
+    };
+
+    handleInput=(event) =>{
+        this.setState({data: this.transformJSON(this.props.data,event.target.value)});
+    };
+
+    render(){
+        return (
+            <div className="widget">
+                <Input handleChange={this.handleInput}/>
+
+                <FolderContainer data={this.state.data}/>
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -7,85 +7,68 @@ require("../styles/application.scss");
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.jsx';
-import Input from './App.jsx';
-
-const folders = [
-    {
-        type: 'dir',
-        name: 'app',
-        children: [
-            {
-                type: 'file',
-                name: 'index.html'
-            },
-            {
-                type: 'dir',
-                name: 'js',
-                children: [
-                    {
-                        type: 'file',
-                        name: 'main.js'
-                    },
-                    {
-                        type: 'file',
-                        name: 'app.js'
-                    },
-                    {
-                        type: 'file',
-                        name: 'misc.js'
-                    },
-                    {
-                        type: 'dir',
-                        name: 'vendor',
-                        children: [
-                            {
-                                type: 'file',
-                                name: 'jquery.js'
-                            },
-                            {
-                                type: 'file',
-                                name: 'underscore.js'
-                            }
-                        ]
-                    }
-                ]
-            },
-            {
-                type: 'dir',
-                name: 'css',
-                children: [
-                    {
-                        type: 'file',
-                        name: 'reset.css'
-                    },
-                    {
-                        type: 'file',
-                        name: 'main.css'
-                    }
-                ]
-            }
-        ]
-    }
-];
 
 
-function filterJson(data,string) {
-    var arr = [];
-    for (var key in data)
-        if (typeof(data[key]) == 'object' && data[key] != null) {
-            if (data['name'].indexOf(string) <= -1) {
-                for (var i = 0; i < data.children.length; i++) {
-                    arr=arr.concat(filterJson(data.children[i], string));
+const folders = {
+    directories: [
+        {
+            type: 'dir',
+            name: 'app',
+            children: [
+                {
+                    type: 'file',
+                    name: 'index.html'
+                },
+                {
+                    type: 'dir',
+                    name: 'js',
+                    children: [
+                        {
+                            type: 'file',
+                            name: 'main.js'
+                        },
+                        {
+                            type: 'file',
+                            name: 'app.js'
+                        },
+                        {
+                            type: 'file',
+                            name: 'misc.js'
+                        },
+                        {
+                            type: 'dir',
+                            name: 'vendor',
+                            children: [
+                                {
+                                    type: 'file',
+                                    name: 'jquery.js'
+                                },
+                                {
+                                    type: 'file',
+                                    name: 'underscore.js'
+                                }
+                            ]
+                        }
+                    ]
+                },
+                {
+                    type: 'dir',
+                    name: 'css',
+                    children: [
+                        {
+                            type: 'file',
+                            name: 'reset.css'
+                        },
+                        {
+                            type: 'file',
+                            name: 'main.css'
+                        }
+                    ]
                 }
-                return arr;
-            }
+            ]
         }
-        else {
-            if (data['name'].indexOf(string) > -1) {
-                arr = arr.concat(data);
-                return arr;
-            }
-        }
-}
+    ]
+};
 
-ReactDOM.render(<App data={folders}/>, document.getElementById('container'));
+
+ReactDOM.render(<App data={folders.directories}/>, document.getElementById('container'));
